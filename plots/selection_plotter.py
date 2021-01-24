5a382c56cf7b0472fa80e852323b5894c51d4484
lukas <np-hardass@protonmail.com>
2021-01-24 15:27:48 +0100

    also plot the center node

--- plots/selection_plotter.py
+++ plots/selection_plotter.py
@@ -10,15 +10,18 @@ def load_data(path):
         return list(reader)
 
 
-def plt_selection(sources, targets, outfile):
+def plt_selection(sources, targets, center, outfile):
     src_lng = [x[0] for x in sources]
     src_lat = [x[1] for x in sources]
     trg_lng = [x[0] for x in targets]
     trg_lat = [x[1] for x in targets]
+    center_lng = [x[0] for x in center]
+    center_lat = [x[1] for x in center]
 
     # plt.hold(True)
-    plt.plot(src_lng, src_lat, 'r.')
+    plt.plot(src_lng, src_lat, 'b.')
     plt.plot(trg_lng, trg_lat, 'g.')
+    plt.plot(center_lng, center_lat, 'r.')
     mplleaflet.show(path=outfile)
 
 
@@ -26,13 +29,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='selection plotter')
     parser.add_argument('--sources', '-s', required=True, type=str)
     parser.add_argument('--targets', '-t', required=True, type=str)
+    parser.add_argument('--center', '-c', required=True, type=str)
     parser.add_argument('--output', '-o', required=True, type=str)
     args = parser.parse_args()
 
     src_file = args.sources
     trg_file = args.targets
+    center_file = args.center
     output_file = args.output
 
     sources = load_data(src_file)
     targets = load_data(trg_file)
-    plt_selection(sources, targets, output_file)
+    center = load_data(center_file)
+    plt_selection(sources, targets, center, output_file)
